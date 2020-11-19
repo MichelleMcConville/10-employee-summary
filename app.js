2f2033a1f3c8289d9385cfe48a182732d602d5ad
Michelle McConville <mchelmconvl@gmail.com>
2020-11-19 15:41:03 -0600

    fixed objects

--- app.js
+++ app.js
@@ -10,74 +10,47 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-const managerQuestions = [
-  {
-    message: "Please select the employee's role?",
-    type: "list",
-    name: "role",
-    choices: ["Manager", "Engineer", "Intern", "Done"],
-  },
-  {
-    message: "Enter the employee's first & last name?",
-    type: "input",
-    name: "name",
-  },
-  {
-    message: "Provide an ID number for the employee?",
-    type: "input",
-    name: "id",
-  },
-  {
-    message: "What is the employee's email address?",
-    type: "input",
-    name: "email",
-  },
-];
+const questions = {
+  message: "Please select one of the following?",
+  type: "list",
+  name: "role",
+  choices: ["Add Manager", "Add Engineer", "Add Intern", "Exit"],
+};
 
-const manager = [
-  {
-    message: "Please provide the manager's office number?",
-    type: "input",
-    name: "office",
-  },
-];
+const manager = {
+  message: "Please provide the manager's office number?",
+  type: "input",
+  name: "office",
+};
 
-const engineer = [
-  {
-    message: "Please enter the engineer's GitHub username?",
-    type: "input",
-    name: "github",
-  },
-];
+const engineer = {
+  message: "Please enter the engineer's GitHub username?",
+  type: "input",
+  name: "github",
+};
 
-const intern = [
-  {
-    message: "Please enter the intern's school name?",
-    type: "input",
-    name: "school",
-  },
-];
+const intern = {
+  message: "Please enter the intern's school name?",
+  type: "input",
+  name: "school",
+};
 
-
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+function start() {
+  employee = [
+    {
+      message: "Enter the employee's first & last name?",
+      type: "input",
+      name: "name",
+    },
+    {
+      message: "Provide an ID number for the employee?",
+      type: "input",
+      name: "id",
+    },
+    {
+      message: "What is the employee's email address?",
+      type: "input",
+      name: "email",
+    },
+  ];
+}
